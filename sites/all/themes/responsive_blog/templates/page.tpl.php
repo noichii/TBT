d789ef326897b61edd3db82d36d9331410314a98
noichii <noelyn_gusi@yahoo.com>
2014-02-20 15:43:22 +0800

    last

--- sites/all/themes/responsive_blog/templates/page.tpl.php
+++ sites/all/themes/responsive_blog/templates/page.tpl.php
@@ -180,15 +180,6 @@
         <?php if (theme_get_setting('breadcrumbs')): ?><?php if ($breadcrumb): ?><div id="breadcrumbs"><?php print $breadcrumb; ?></div><?php endif;?><?php endif; ?>
         <?php print $messages; ?>
         <?php if ($page['content_top']): ?><div id="content_top"><?php print render($page['content_top']); ?></div><?php endif; ?>
-        <div id="content-wrap">
-          <?php print render($title_prefix); ?>
-          <?php if ($title): ?><h1 class="page-title"><?php print $title; ?></h1><?php endif; ?>
-          <?php print render($title_suffix); ?>
-          <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
-          <?php print render($page['help']); ?>
-          <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-          <?php print render($page['content']); ?>
-        </div>
       </section> <!-- /#main -->
     </div>
 
